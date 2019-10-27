13c1fda584ec9d02555cce9dadb4ce02e36f394e
Jacob <jathoma98@gmail.com>
2019-10-27 11:33:22 -0400

    init of SimpleLab

---
+++ src/main/java/com/org/simplelab/SimpleLabApplication.java
@@ -0,0 +1,13 @@
+package com.org.simplelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/simplelab/SimpleLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.simplelab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
