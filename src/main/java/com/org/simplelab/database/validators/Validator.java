f2c017150bba632bb5311eba79cfb9502d9a3cba
Jacob <jathoma98@gmail.com>
2019-11-05 15:35:19 -0500

    Added salting to hash function. Added documentation to Validators.

--- src/main/java/com/org/simplelab/database/validators/Validator.java
+++ src/main/java/com/org/simplelab/database/validators/Validator.java
@@ -1,17 +1,34 @@
 package com.org.simplelab.database.validators;
 
+/**
+ * Validator classes are used to ensure that data that the user
+ * sends to the server is properly formatted.
+ * @author Jacob Thomas
+ */
 public abstract class Validator {
 
+    /**
+     * Exception to be thrown, which should contain the error message
+     * corresponding to how field data is incorrect.
+     */
     public class InvalidFieldException extends Exception{
         public InvalidFieldException(String message){
             super(message);
         }
     }
 
-
+    //probably wont be used
     protected boolean validated = false;
 
+    /**
+     * Method which checks each field that the user sends, to ensure they are properly formatted.
+     * @throws InvalidFieldException - contains a String detailing all errors in user data.
+     */
     public abstract void validate() throws InvalidFieldException;
 
+    /**
+     * Builds the object corresponding to the validator.
+     * @return the object to be built
+     */
     public abstract Object build();
 }
