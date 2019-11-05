30e5def43f915432fe74d4631050e2bb788e89b8
peilin zhu <zpl969129933@gmail.com>
2019-11-05 16:33:49 -0500

    studentInfo

--- src/main/resources/static/js/sudentInfo.js
+++ src/main/resources/static/js/sudentInfo.js
@@ -1,11 +1,6 @@
-$(document).ready(function () {
-    $(".userName").load("/studentInfo", function(){
-        $(".userName").text("Hi, " + $(".userName").text());
-    })
-});
 
-$(document).ready(function (){
-    $("#newGame").click(function(){
-        window.location.href= "game.html";
-    })
+$.get("/info/get_user", function(user) {
+    console.log(user.firstname)
+    $(document).ready(function () {
+    });
 });
