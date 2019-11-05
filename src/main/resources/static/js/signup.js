4ed4bdf681f3e1697a14c7e9731e68babffb577d
Jacob <jathoma98@gmail.com>
2019-11-05 14:42:44 -0500

    refactored signup.js to use backend validation

--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -1,10 +1,6 @@
 function signupbtnEvent(){
     let user_role = $("#role").val;
     let password = $("#sp_password").val(), repassword = $("#sp_re_password").val();
-    if (password !== repassword){
-        console.log("pw != rpw")
-        return "error"
-    }
     user_data = {
         userName : $("#userName").val(),
         email: $("#email").val(),
@@ -15,23 +11,20 @@ function signupbtnEvent(){
         answer: $("#answer").val(),
         identity: user_role
     }
-    for(let key in user_data){
-        if(user_data[key] == undefined || user_data[key].lenght == 0){
-            console.log("There is a empty field: " + key);
-            return "error";
-        }
-    }
     console.log(user_data);
     $.post("/signup/userdata", user_data, function (result) {
         if (result.success === "true") {
             window.location.href= "/login";
         }
         else {
-            if (result.reason === "password does not match"){
+            if (result.error === "password does not match"){
                 console.log("The password fields do not match. Please re-enter your passwords");
             }
-            else if (result.reason === "username taken"){
+            else if (result.error === "username taken"){
                 console.log("The username is already taken");
+            }
+            else{
+                console.log(result.error);
             }
         }
     })
