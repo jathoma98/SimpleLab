e9ec1f29bf6cf049da4f623c5daa4afd10f9de24
jathoma98 <34799315+jathoma98@users.noreply.github.com>
2019-11-06 18:08:53 -0500

    Fixed null pointer exception in TeacherController.java

--- src/main/java/com/org/simplelab/controllers/TeacherController.java
+++ src/main/java/com/org/simplelab/controllers/TeacherController.java
@@ -3,6 +3,7 @@ package com.org.simplelab.controllers;
 import com.org.simplelab.database.CourseDB;
 import com.org.simplelab.database.entities.Course;
 import com.org.simplelab.database.entities.DummyEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +15,13 @@ import java.util.List;
 @Controller
 @RequestMapping(path="/teacher")
 public class TeacherController {
+
+    @Autowired
+    CourseDB cdb;
+
     @RequestMapping("")
     public String root(HttpSession session, Model model) {
-        CourseDB cdb = new CourseDB();
+        //CourseDB cdb = new CourseDB();
         List<Course> list_course = cdb.getCoursesForTeacher((String)session.getAttribute("user_id"));
         String home_navig = ((String)session.getAttribute("username")) + "'s Home";
         model.addAttribute("home_navig", home_navig);
