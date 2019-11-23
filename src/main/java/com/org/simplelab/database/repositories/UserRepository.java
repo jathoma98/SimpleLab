2c78cdd238cc68332babd597e80943fdb83414d0
Zihao Zheng <zhzheng9211@gmail.com>
2019-11-23 14:01:18 -0500

    save

--- src/main/java/com/org/simplelab/database/repositories/UserRepository.java
+++ src/main/java/com/org/simplelab/database/repositories/UserRepository.java
@@ -21,9 +21,12 @@ public interface UserRepository extends BaseRepository<User> {
     public void deleteByUsername(String username);
 
 
-    @Query(nativeQuery = true, value = "SELECT id, username, created_date, email, firstname, lastname, role  " +
-            "FROM simplelab.user " +
-            "WHERE username OR institution OR email OR firstname OR lastname LIKE '%:keyword%'")
+//    @Query(nativeQuery = true, value = "SELECT id, username, created_date, email, firstname, lastname, role  " +
+//            "FROM simplelab.user " +
+//            "WHERE username OR institution OR email OR firstname OR lastname LIKE '%:keyword%'")
+    @Query(nativeQuery = true, value =
+            "SELECT * FROM simplelab.user " +
+            "WHERE  username LIKE '%:keyword%' OR institution LIKE '%:keyword%'")
     public List<User> searchUserWithKeyword(@Param("keyword") String keyword);
 
 
