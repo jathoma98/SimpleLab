51826ec66dd1feace134e03fd3029b6ca8e63932
Zihao Zheng <zhzheng9211@gmail.com>
2019-12-08 09:53:05 -0500

    save

--- src/main/java/com/org/simplelab/database/entities/Recipe.java
+++ src/main/java/com/org/simplelab/database/entities/Recipe.java
@@ -10,14 +10,10 @@ public class Recipe extends BaseTable {
 
     private String name;
 
-    /*
-    * Equipment_1
-    *
-    * Equipment_2
-    *
-    * Result_Equipment
-    *
-    *
+    /*Todo: -Zee
+    *   Equipment_1
+    *   Equipment_2
+    *   Result_Equipment
     * */
 
 
