f7382563efa15cdd13c258ef84996af85fc8e433
Jacob <jathoma98@gmail.com>
2019-12-09 14:29:14 -0500

    Added documentation to interaction handler

--- src/main/java/com/org/simplelab/controllers/DoLabController.java
+++ src/main/java/com/org/simplelab/controllers/DoLabController.java
@@ -31,8 +31,8 @@ public class DoLabController extends BaseController {
 
 
     public static final String DO_LAB_BASE_MAPPING = "/doLab";
-    public static final String LAB_ID_MAPPING = "/{instance_id}";
-    public static final String INTERACTION_MAPPING  = LAB_ID_MAPPING + "/interaction";
+    public static final String LAB_ID_MAPPING = "/{lab_id}";
+    public static final String INTERACTION_MAPPING  = "/{instance_id}";
 
     /**
      * Initiates building of a LabInstance document, which will record user actions and state of a lab in progress.
@@ -81,6 +81,7 @@ public class DoLabController extends BaseController {
      *                           In the previous example, the beaker the User clicks on is object2
      *     parameter: String -- Represents parameter for interaction.
      *                          ex: If we want to pour 100ml liquid from beaker 1 to beaker 2, parameter = 100.
+     *     stepNum: int -- current Step the user is on.
      * }
      *
      * @return:
@@ -111,8 +112,6 @@ public class DoLabController extends BaseController {
      * RRO with: {
      *     success: true
      *     action: COMPLETE_LAB
-     *     data: TODO: discuss this, probably just return a grade or something
-     *     //TODO: May be just return how time student make invalid recipes - Zee
      * }
      */
 
@@ -121,20 +120,39 @@ public class DoLabController extends BaseController {
                                           @RequestBody EquipmentInteractionDTO dto){
 
         RRO<Equipment[]> rro = new RRO<>();
+        /**
+         * First, find the LabInstance we created for this lab when we called
+         * getLabToDo() method above, which initialized the workspace and lab history.
+         */
         LabInstance currentInstance = instanceDB.findById(instance_id);
 
+        /**
+         * Load the equipment and parameters which were sent from the client.
+         * The parameter determines how the interaction will be performed.
+         * ex: if we want use a heater to heat something, the parameter is the increase in temperature.
+         */
         Equipment eq1 = dto.getObject1();
         Equipment eq2 = dto.getObject2();
         String parameter = dto.getParameter();
 
-        //for saving interaction
+        /**
+         * Perform the interaction according to the Interaction interface in eq1, which is loaded
+         * based on the "type" value.
+         * ex: If an equipment has type "heat", the Heat interface is loaded automatically when we
+         * pull from DB and performed here.
+         */
         Equipment eq2_original = eq2;
         Equipment result = eq1.getInteraction().interactWith(eq2, parameter);
         if (result.exists()){
             eq2 = result;
         }
 
-        //save the interaction in labinstance
+        /**
+         * After we perform the interaction, save the info about the interaction in MongoDB.
+         * Each step has a StepRecord object in the LabInstance. The StepRecord has a list of
+         * StepActions. StepAction records information about every interaction the user makes
+         * in each step.
+         */
         DoLabEventHandler.InteractionObjects interactionInfo = new DoLabEventHandler.InteractionObjects();
         interactionInfo.setEq1(eq1);
         interactionInfo.setEq2(eq2);
@@ -151,20 +169,41 @@ public class DoLabController extends BaseController {
             // do something
         }
 
+        /**
+         * Now we load the current step from the Lab that this instance manages.
+         */
         Lab currentLab = SerializationUtils.deserialize(currentInstance.getSerialized_lab());
         Step currentStep =  currentLab.getSteps()
                              .stream()
                              .filter(step -> step.getStepNum() == dto.getStepNum())
                              .collect(Collectors.toList()).get(0);
 
-        //Check if the object is equal to the current target object.
+        /**
+         * After loading the current step, check if the object that resulted from the
+         * interaction is equal to the target object for this step.
+         */
         if (eq2.equals(currentStep.getTargetObject())){
-            //if it matches the target object, check if this is the last step
+            /**
+             * If it matches, check if the current step is the last step in the lab.
+             */
             rro.setData(null);
             if (dto.getStepNum() == currentLab.returnLastStepNumber()){
+                /**
+                 * If the step is complete and the step is the last step in the lab,
+                 * the lab is complete. We finalize the lab by assigning a grade,
+                 * and setting the variable finished = true.
+                 * RRO returns COMPLETE_LAB to indicate to the UI that the lab is done.
+                 */
                 eventHandler.finalizeInstance(currentInstance);
                 rro.setAction(RRO.LAB_ACTION_TYPE.COMPLETE_LAB.name());
             } else {
+                /**
+                 * Otherwise, we create a new StepRecord object and add it to the list
+                 * of StepRecords in the LabInstance. This is because we are advancing to the next
+                 * step, so we should save future user actions in the object for the next step.
+                 * We also set RRO to ADVANCE_STEP to indicate to the UI that we should go to the
+                 * next step.
+                 */
                 eventHandler.advanceStep(currentInstance);
                 rro.setAction(RRO.LAB_ACTION_TYPE.ADVANCE_STEP.name());
             }
