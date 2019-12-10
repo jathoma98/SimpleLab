b42677596885019f8f77c0fcf24adfafa1de7945
Zihao Zheng <zhzheng9211@gmail.com>
2019-12-10 12:20:55 -0500

    add three field in step

---
+++ src/main/java/com/org/simplelab/database/repositories/sql/StepRepository.java
@@ -0,0 +1,16 @@
+package com.org.simplelab.database.repositories.sql;
+
+import com.org.simplelab.database.entities.sql.Step;
+import com.org.simplelab.database.services.projections.Projection;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional
+public interface StepRepository extends BaseRepository<Step>{
+
+//
+//    List<Step>
+}
