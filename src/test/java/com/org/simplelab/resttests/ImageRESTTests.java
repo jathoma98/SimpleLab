fe37353ef517b9abcb5bbf8146801afb9d0a425d
Jacob <jathoma98@gmail.com>
2019-12-14 16:59:30 -0500

    Added get and insert methods

--- src/test/java/com/org/simplelab/resttests/ImageRESTTests.java
+++ src/test/java/com/org/simplelab/resttests/ImageRESTTests.java
@@ -1,17 +1,22 @@
 package com.org.simplelab.resttests;
 
 
+import com.org.simplelab.database.entities.sql.files.ImageFile;
+import com.org.simplelab.database.validators.ImageFileValidator;
 import com.org.simplelab.restcontrollers.ImageFileRESTController;
 import com.org.simplelab.restrequest.RESTRequest;
+import com.org.simplelab.utils.DBTestUtils;
+import com.org.simplelab.utils.JSONBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.io.File;
+import java.util.Arrays;
+
+import static com.org.simplelab.restrequest.RESTRequest.RequestType.GET;
+import static com.org.simplelab.restrequest.RESTRequest.RequestType.POST;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ImageRESTTests extends RESTTestBaseConfig {
 
@@ -25,15 +30,33 @@ public class ImageRESTTests extends RESTTestBaseConfig {
     @Test
     @WithMockUser(username = username, password = username)
     void testFileUpload() throws Exception{
-        String fileName = "test.txt";
-        File file = new File(fileName);
-        file.delete();
+        ImageFileValidator v = new ImageFileValidator();
+        v.setFileName("testfile");
+        v.setFileType(".png");
+        v.setData("test data".getBytes());
+        v.set_metadata(metadata);
 
-        MockMultipartFile mockMultipartFile = new MockMultipartFile("user-file", fileName, "text/plain", "test data".getBytes());
+        imageRequest.sendData(POST, "/upload", JSONBuilder.asJson(v))
+                .andExpectSuccess(true);
 
-        mvc.perform(MockMvcRequestBuilders.multipart(ImageFileRESTController.BASE_MAPPING + "/upload").file(mockMultipartFile))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andDo(MockMvcResultHandlers.print());
+        ImageFile found = imageDB.getRepository().findBy_metadata(metadata).get(0);
+        assertTrue(found.getId() > 0);
+        assertEquals(found.getFileName(), v.getFileName());
+        assertEquals(found.getFileType(), v.getFileType());
+        assertTrue(Arrays.equals(found.getData(), v.getData()));
+    }
+
+    @Test
+    @WithMockUser(username = username, password = username)
+    void testFileGet() throws Exception{
+        ImageFile file = new ImageFile();
+        file.setFileType("text");
+        file.setFileName("test");
+        file.setData("test data".getBytes());
+        long file_id = DBTestUtils.insertAndGetId(file, imageDB);
+
+        imageRequest.send(GET, "/" + Long.toString(file_id))
+            .andExpectData(JSONBuilder.asJson(file));
 
     }
 
