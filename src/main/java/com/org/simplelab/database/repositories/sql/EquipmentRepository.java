eda9fa3582d62c993e6dd9d75193ef56539c33c4
peilin zhu <zpl969129933@gmail.com>
2019-12-15 14:50:15 -0500

    Delete Equipment Error, Post image byte

--- src/main/java/com/org/simplelab/database/repositories/sql/EquipmentRepository.java
+++ src/main/java/com/org/simplelab/database/repositories/sql/EquipmentRepository.java
@@ -1,6 +1,10 @@
 package com.org.simplelab.database.repositories.sql;
 
+import com.org.simplelab.database.entities.sql.Course;
 import com.org.simplelab.database.entities.sql.Equipment;
+import com.org.simplelab.database.entities.sql.Lab;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +16,9 @@ public interface EquipmentRepository extends BaseRepository<Equipment> {
 
     <T> List<T> findByCreator_id(long id, Class<T> projection);
     List<Equipment> findByCreator_id(long id);
+
+    @Query(value =
+            "SELECT lab_id from lab_equipments \n" +
+                    "WHERE lab_equipments.equipments_id = :equip_id", nativeQuery = true)
+    public List<String> checkUsingEquipment(@Param("equip_id") long equip_id);
 }
